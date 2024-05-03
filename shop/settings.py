ad929e95f2fca104f6eac02181eb6f06e3a48e0a
Nikita Filipenkov <nikismol94@yandex.ru>
2024-05-03 17:45:45 +0300

    fix: setting.py

--- shop/settings.py
+++ shop/settings.py
@@ -91,9 +91,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static_dev')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 MEDIA_URL = '/media/'
